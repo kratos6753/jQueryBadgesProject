7e76f305df805417fb068d5fa35ca1fdb2ffc693
Chiranjeevi K <chiru6753@gmail.com>
2016-12-12 00:36:42 +0530

    made changes in main.js file to complete the project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
-
-  // your code will go here
+	var report_card_url = 'https://www.codeschool.com/users/kratos6753.json';
+	$.ajax({
+		method: "GET",
+		url: report_card_url,
+		dataType: 'jsonp'
+	}).done(function(response) {
+		var courses_completed = response.courses.completed;
+		var badges = $('#badges');
+		courses_completed.forEach(function(course) {
+			badges.append("<div class='course'></div>");
+			var course_div = badges.find('.course').last()[0];
+			var h3 = $("<h3></h3>").html(course.title);
+			course_div.append(h3[0]);
+			var img = $("<img />").attr('src', course.badge);
+			course_div.append(img[0]);
+			var course_link = $("<a></a>").attr({'href': course.url, 'target': '_blank', 'class': 'btn btn-primary'}).text('See Course');
+			course_div.append(course_link[0]);
+		});
+	});
 
 });
