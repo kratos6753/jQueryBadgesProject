9746e8f98de9b3865b4982a94e31c43dbba63571
Chiranjeevi K <chiru6753@gmail.com>
2016-12-12 00:59:49 +0530

    doing it the easy way

--- assets/main.js
+++ assets/main.js
@@ -8,14 +8,10 @@ $(function() {
 			var courses_completed = response.courses.completed;
 			var badges = $('#badges');
 			courses_completed.forEach(function(course) {
-				var course_div = $("<div></div>").attr('class', 'course')[0];
-				badges.append(course_div);
-				var h3 = $("<h3></h3>").html(course.title).attr('title', course.title);
-				course_div.append(h3[0]);
-				var img = $("<img />").attr('src', course.badge);
-				course_div.append(img[0]);
-				var course_link = $("<a></a>").attr({'href': course.url, 'target': '_blank', 'class': 'btn btn-primary'}).text('See Course');
-				course_div.append(course_link[0]);
+				var course_div = $("<div />", {'class': 'course'}).appendTo(badges);
+				$("<h3 />", {text: course.title}).appendTo(course_div);
+				$("<img />", {'src': course.badge}).appendTo(course_div);
+				$("<a />", {'href': course.url, 'target': '_blank', 'class': 'btn btn-primary', text: 'See Course'}).appendTo(course_div);
 			});
 		}
 	});
