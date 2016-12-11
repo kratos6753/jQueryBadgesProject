1c86a604c5003c03950510cb5ff0d49502dc796d
Chiranjeevi K <chiru6753@gmail.com>
2016-12-12 00:43:10 +0530

    modified some code to try to pass the project

--- assets/main.js
+++ assets/main.js
@@ -3,20 +3,20 @@ $(function() {
 	$.ajax({
 		method: "GET",
 		url: report_card_url,
-		dataType: 'jsonp'
-	}).done(function(response) {
-		var courses_completed = response.courses.completed;
-		var badges = $('#badges');
-		courses_completed.forEach(function(course) {
-			badges.append("<div class='course'></div>");
-			var course_div = badges.find('.course').last()[0];
-			var h3 = $("<h3></h3>").html(course.title);
-			course_div.append(h3[0]);
-			var img = $("<img />").attr('src', course.badge);
-			course_div.append(img[0]);
-			var course_link = $("<a></a>").attr({'href': course.url, 'target': '_blank', 'class': 'btn btn-primary'}).text('See Course');
-			course_div.append(course_link[0]);
-		});
+		dataType: 'jsonp',
+		success: function(response) {
+			var courses_completed = response.courses.completed;
+			var badges = $('#badges');
+			courses_completed.forEach(function(course) {
+				var course_div = $("<div></div>").attr('class', 'course')[0];
+				badges.append(course_div);
+				var h3 = $("<h3></h3>").html(course.title);
+				course_div.append(h3[0]);
+				var img = $("<img />").attr('src', course.badge);
+				course_div.append(img[0]);
+				var course_link = $("<a></a>").attr({'href': course.url, 'target': '_blank', 'class': 'btn btn-primary'}).text('See Course');
+				course_div.append(course_link[0]);
+			});
+		}
 	});
-
 });
